570586967cbfb5ff00c71175ecdb59f12dad4f63
luis08 <luis08@msn.com>
2019-11-11 19:17:15 -0500

    Use ComponentScan

--- src/main/java/com/appraisers/app/assignments/AssignmentsApplication.java
+++ src/main/java/com/appraisers/app/assignments/AssignmentsApplication.java
@@ -1,9 +1,12 @@
 package com.appraisers.app.assignments;
 
+import com.appraisers.resources.AssignmentResource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses = AssignmentResource.class)
 public class AssignmentsApplication {
 
 	public static void main(String[] args) {
