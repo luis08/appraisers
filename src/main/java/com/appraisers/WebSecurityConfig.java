23b1965cd503e5051925674b8b5b4db47e1255d1
luis08 <luis08@msn.com>
2019-11-15 19:06:02 -0500

    Allow index.html without security

--- src/main/java/com/appraisers/WebSecurityConfig.java
+++ src/main/java/com/appraisers/WebSecurityConfig.java
@@ -11,5 +11,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
+        http.authorizeRequests()//
+                .antMatchers("/index.html").permitAll()//
+                // Disallow everything else..
+                .anyRequest().authenticated();
     }
 }
