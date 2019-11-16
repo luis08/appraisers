1521e9ecc09f22d0f8af9affbdbacbc2df4243bd
luis08 <luis08@msn.com>
2019-11-15 19:47:19 -0500

    Disable security for the moment

--- src/main/java/com/appraisers/WebSecurityConfig.java
+++ src/main/java/com/appraisers/WebSecurityConfig.java
@@ -12,8 +12,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.authorizeRequests()//
-                .antMatchers("/").permitAll()//
-                // Disallow everything else..
-                .antMatchers("/assignment**/**").authenticated();
+                .antMatchers("/").permitAll();
     }
 }
