14eaf1fecc8684e355e9e7a42c68333851392d15
luis08 <luis08@msn.com>
2020-06-25 18:25:25 -0400

    Notes

--- src/main/java/com/appraisers/WebSecurityConfig.java
+++ src/main/java/com/appraisers/WebSecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        // SSL https://frugalisminds.com/spring-boot/how-to-configure-ssl-in-spring-boot-2/
         //Oauth2 https://spring.io/guides/tutorials/spring-boot-oauth2/
         http.csrf().disable();
         http.authorizeRequests()
